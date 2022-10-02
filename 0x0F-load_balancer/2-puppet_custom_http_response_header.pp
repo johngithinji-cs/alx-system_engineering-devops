4eb612ada97b22a2a9e2da931bf16f429dca1ef4
johngithinji-cs <mathaijohn97@gmail.com>
2022-10-02 08:49:47 +0300

    updates

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,12 +1,30 @@
-#!/usr/bin/env bash
-# install load balancer
-sudo apt-get -y install software-properties-common
-sudo add-apt-repository -y ppa:vbernat/haproxy-1.6
-sudo apt-get -y update
-sudo apt-get -y install haproxy=1.6.\*
-sudo echo -e "\nlisten www" >> /etc/haproxy/haproxy.cfg
-sudo echo -e "\tbind :80" >> /etc/haproxy/haproxy.cfg
-sudo echo -e "\tbalance roundrobin" >> /etc/haproxy/haproxy.cfg
-sudo echo -e "\tserver gc-499-web-01-1553637534 34.73.5.42" >> /etc/haproxy/haproxy.cfg
-sudo echo -e "\tserver gc-499-web-02-1554448076 34.73.70.108" >> /etc/haproxy/haproxy.cfg
-sudo service haproxy restart
+# Setup New Ubuntu server with nginx
+# and add a custom HTTP header
+
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+	ensure => 'installed',
+	require => Exec['update system']
+}
+
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
+}
+
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://webcodebeast.tech/ permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
+
+exec {'HTTP header':
+	command => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
+
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
+}
